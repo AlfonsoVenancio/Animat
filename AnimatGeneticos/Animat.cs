0bfc05a59a1b1fb21d12f5bc259389a317b13709
Alfonso Venancio <alfonso.venancio22@gmail.com>
2018-11-27 09:26:15 -0600

    Inicializacion corregida del tablero

--- AnimatGeneticos/Animat.cs
+++ AnimatGeneticos/Animat.cs
@@ -6,5 +6,6 @@ using System.Threading.Tasks;
 
 namespace AnimatGeneticos {
     class Animat {
+        int n, m;
     }
 }
--- AnimatGeneticos/Tablero.cs
+++ AnimatGeneticos/Tablero.cs
@@ -7,17 +7,34 @@ using System.Threading.Tasks;
 namespace AnimatGeneticos{
     class Tablero {
         enum valorCasilla { bloque, izquieda, derecha, arriba, abajo, bomba, animat};
-        List<valorCasilla>[,] tablero;
-        int N, M;
-        void Main(String[] args) {
-            N = int.Parse(args[0]);
-            M = int.Parse(args[1]);
+        static List<valorCasilla>[,] tablero;
+        static int N, M;
+
+        static void Main(String[] args) {
+            //N = int.Parse(args[0]);
+            //M = int.Parse(args[1]);
+            N = 7;
+            M = 7;
             poblateTablero();
+            for (int i = 0; i < N; i++) {
+                Console.WriteLine("--------------CAMBIO DE FILA: " + i + "-----------------");
+                for (int j = 0; j < M; j++) {
+                    Console.WriteLine("--------------CAMBIO DE COLUMNA: " + j + "-----------------");
+                    foreach (valorCasilla valor in tablero[i, j]) {
+                        Console.WriteLine(valor);
+                    }
+                }
+            }
         }
-        void poblateTablero() {
+        static void poblateTablero() {
             tablero = new List<valorCasilla>[N,M];
             Random generadorRandom = new Random();
             int contadorBombas = 0;
+            for(int i = 0; i < N; i++) {
+                for(int j = 0; j < M; j++) {
+                    tablero[i, j] = new List<valorCasilla>();
+                }
+            }
             //Bloques
             for (int i = 1; i < N; i+=2) {
                 for(int j = 1; j < M; j += 2) {
@@ -28,8 +45,10 @@ namespace AnimatGeneticos{
             for(int i = 0; i < N; i++) {
                 for(int j = 0; j < M; j++) {
                     if (i % 2 == 0) {
-                        if (i % 4 == 0 && j<M-1) {
-                            tablero[i, j].Add(valorCasilla.derecha);
+                        if (i % 4 == 0) {
+                            if (j < M - 1) {
+                                tablero[i, j].Add(valorCasilla.derecha);
+                            }
                         }
                         else if(j>0){
                             tablero[i, j].Add(valorCasilla.izquieda);
@@ -41,9 +60,10 @@ namespace AnimatGeneticos{
             for (int i = 0; i < N; i++) {
                 for(int j = 0; j < M; j++) {
                     if (j % 2 == 0) {
-                        if (j % 4 == 0 && i>0) {
-                            tablero[i, j].Add(valorCasilla.arriba);
-
+                        if (j % 4 == 0) {
+                            if (i > 0) {
+                                tablero[i, j].Add(valorCasilla.arriba);
+                            }
                         }else if (i < N - 1) {
                             tablero[i, j].Add(valorCasilla.abajo);
                         }
@@ -54,7 +74,7 @@ namespace AnimatGeneticos{
             while (contadorBombas < N * M * 0.3) { 
                 int x = generadorRandom.Next(0, N);
                 int y = generadorRandom.Next(0, M);
-                if (!tablero[x, y].Contains(valorCasilla.bloque)) {
+                if (!tablero[x, y].Contains(valorCasilla.bloque) && !tablero[x,y].Contains(valorCasilla.bomba)) {
                     tablero[x, y].Add(valorCasilla.bomba);
                     contadorBombas++;
                 }
